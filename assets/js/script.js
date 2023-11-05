663e3db754d1dc4733747977320eaf23e103503f
RomaP28 <posokhovrom@gmail.com>
2023-11-05 22:20:59 +0200

    added burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 	
+	const burger = document.querySelector('.burger');
+	const headerMenu = document.querySelector('header .menu');
+
+	burger?.addEventListener('click', ()=>{
+		[headerMenu, burger].forEach(el=> el.classList.toggle('open'));
+		burger.classList.contains('open') ? remove_body_scroll() : add_body_scroll();
+	});
+	
+	let scrollY;
+	function remove_body_scroll() {
+		scrollY = window.scrollY;
+		const addHeight = () => {
+			document.documentElement.classList.add("lock");
+			clearTimeout(timeId);
+		};
+		const timeId = setTimeout(addHeight, 500);
+	}
+	
+	function add_body_scroll() {
+		document.documentElement.classList.remove("lock");
+		window.scrollTo(0, scrollY);
+	}
+	
 	new Swiper('.swiper', {
 		direction: 'horizontal',
 		loop: true,
